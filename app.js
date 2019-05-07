7c1804aae3abc71f87051792a81e1f067832da95
KIMINJOONG <ijboym216@gmail.com>
2019-05-07 18:09:49 +0900

    static directory setting

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ app.listen(3000, () => {
   console.log("start!! express server on port 3000");
 });
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get("/main", (req, res) => {
   res.sendFile(__dirname + "/public/main.html");
 });
